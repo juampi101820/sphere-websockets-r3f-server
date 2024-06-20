48dbbc05277f935ba1c73ef16e6c11b64a8c000b
Juan Pablo Marin Cifuentes <juan.marin@studiof.com.co>
2024-06-19 20:44:59 -0500

    feat: escuchar eventos de estados corazon

--- src/socketManager.js
+++ src/socketManager.js
@@ -1,14 +1,14 @@
 "use strict";
 
-function handleSocketConnection(socket, io, doorStates) {
+function handleSocketConnection(socket, io, states) {
   console.log(
     "Player joined with ID",
     socket.id,
     ". There are " + io.engine.clientsCount + " players connected."
   );
 
-  // Enviar el estado actual de las puertas al nuevo jugador
-  socket.emit("update-door-states", doorStates);
+  // Enviar el estado actual al nuevo jugador
+  socket.emit("update-states", states);
 
   socket.on("player-moving", (transforms) => {
     socket.broadcast.emit("player-moving", transforms);
@@ -16,8 +16,21 @@ function handleSocketConnection(socket, io, doorStates) {
 
   // Manejar cambios en el estado de las puertas
   socket.on("change-door-state", (newDoorStates) => {
-    Object.assign(doorStates, newDoorStates);
-    io.emit("update-door-states", doorStates);
+    Object.assign(states.doorStates, newDoorStates);
+    io.emit("update-states", states);
+  });
+
+  // Manejar cambios en el estado de las monedas
+  socket.on("change-coin-state", (newCoinStates) => {
+    Object.assign(states.coinStates, newCoinStates);
+    io.emit("update-states", states);
+  });
+
+   // Manejar cambios en el estado de los coraziones
+   socket.on("change-heart-state", (newHeartStates) => {
+    console.log("eventos corazon", newHeartStates);
+    Object.assign(states.heartStates, newHeartStates);
+    io.emit("update-states", states);
   });
 
   socket.on("disconnect", () => {
